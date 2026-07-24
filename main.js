4e1e1b718f74e896ca29728620aa0e78bf620de4
unknown <tluraschi@escuelasproa.edu.ar>
2026-07-24 08:39:34 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let botoncancha = document.getElementById("mostrarcancha");
+
+let lista = document.getElementById("listacancha");
+
+botoncancha.addEventListener("click", function (){
+
+    lista.innerHTML = `
+        <li>HTML</li>
+        
+
+    `;
+});
